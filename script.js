92f616342e42f3febe2b90544a0e9ba8f10ac66d
LatifahAlGhamdi <Latifah.Dafer.abt.js@tuwaiq.edu.sa>
2021-10-18 19:18:51 +0300

    Done HW.

---
+++ script.js
@@ -0,0 +1,5 @@
+const movie = {
+    title: "Puff the Magic Dragon", duration: 30,
+    stars: ["Puff", "Jackie", "Living Sneezes."]
+}
+console.log(movie.title + " lasts for " + movie.duration + " minutes. Stars: " + movie.stars.join(','))
