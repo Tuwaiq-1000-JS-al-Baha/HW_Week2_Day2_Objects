6c00a36a417068231f841e00ad13b1b57e59944f
Mohammed-Dukhaykh <Mohammed.Obaidallah.abt.js@tuwaiq.edu.sa>
2021-10-18 20:05:48 +0300

    Homework W2D2

---
+++ script.js
@@ -0,0 +1,7 @@
+const movie = {
+    title: 'ice road' ,
+    duration: 130 ,
+    stars: ['John' , 'Mike' , 'Layla' , 'Sara' , 'the rock']
+}
+
+console.log(`${movie.title} lasts for ${movie.duration} minutes.Stars:${movie.stars}`)
