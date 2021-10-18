a909993a21cc777841e35ec83060ecb20e946625
ReemAbdullah1420 <Reem.Abdullah.abt.js@tuwaiq.edu.sa>
2021-10-18 17:59:28 +0300

    HW

---
+++ hw.js
@@ -0,0 +1,5 @@
+const movie = {
+    title: "Shutter island", duration: 138, stars: ["Leonardo Dicaprio",
+        "Mark Ruffalo", "michelle Williams", "Max Von Sydow"]
+}
+{ console.log(movie.title + " lasts for " + movie.duration + " minutes. " + "Stars: " + movie.stars.join()) }
