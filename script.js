d12c2ad9cc78dfe60fb10fa2c28be339cc643520
Arwa94a <92246034+Arwa94a@users.noreply.github.com>
2021-10-18 10:25:16 -0700

    I done

---
+++ script.js
@@ -0,0 +1,10 @@
+const myFavoritMovie={
+    title:'blindspot',
+    duration:1,
+    stars:['jone','smith','sia','taylor']
+    //Puff the Magic Dragon lasts for 30 minutes.
+    // Stars: Puff, Jackie, Living Sneezes
+
+}
+console.log('like so :',myFavoritMovie.title,'lasts for',myFavoritMovie.duration,'hour',
+'stars:',myFavoritMovie.stars.join())
