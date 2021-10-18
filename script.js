0f8dcef8ecfeeec60f74469e91dce72f1bbd7669
Ghadah-Mohammed <92169172+Ghadah-Mohammed@users.noreply.github.com>
2021-10-19 00:20:47 +0300

    HW.day2

---
+++ script.js
@@ -0,0 +1,6 @@
+const store ={
+    title:"preson break",
+    duration:55,
+    stars:["wintroth,","domonic,","sara."]  
+}
+console.log("the" ,store.title ,"lasts for",store.duration,"stars",store.stars[0],store.stars[1],store.stars[2])
