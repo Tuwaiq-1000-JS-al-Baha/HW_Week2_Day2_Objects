5a31f74141a3b2247b5a399f58b7ca9d10c30b08
Meshoeternity <Mashael.othman.abt.js@tuwaiq.edu.sa>
2021-10-18 21:44:49 +0300

    home work done

---
+++ hwd2w2.js
@@ -0,0 +1,12 @@
+const movie =
+
+     { title:"Taare Zameen par",
+    duration:30,
+    stars:[
+         " AmirKhan " ," TanayChheda ","DarsheelSafary . "]}
+     {
+        console.log ( "My Favorite movie is The "+movie.title + " lasts for "+ movie.duration +" minutes"+" . "+ " Stars :" + movie .stars)}
+
+     
+        
+    
