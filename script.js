56e6f7f454c68af05ab48f11f3fb2f8b0bcac245
RahmaJumaan55 <92191130+RahmaJumaan55@users.noreply.github.com>
2021-10-18 23:15:11 +0300

    Done

---
+++ script.js
@@ -0,0 +1,8 @@
+const favoritemovie= {
+    title :"Puff the Magic Dragon",
+    duration : 30 ,
+    stars : ["Puff" , "Jackie" , "Living " , "Sneezes."]
+  }
+for(i=0 ; i < favoritemovie.stars.length ; i++){
+console.log(favoritemovie.title + " " +"lasts for" + " "+ favoritemovie.duration + " "+"minutes." + "Stars:" + favoritemovie.stars)
+    }
