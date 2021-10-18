c7167309da2e068717e4da624d12a9de7dd3f749
Abeer5011 <92170094+Abeer5011@users.noreply.github.com>
2021-10-18 19:29:22 +0300

    HWDone

---
+++ script.js
@@ -0,0 +1,9 @@
+const myFavoriteMovie = {
+    title: 'Spider man far from home',
+    duration: 130,
+    stars: ['Tom Holland', 'Jake Gyllenhaal', 'Samuel L. Jackson', 'Marisa Tomei']
+
+}
+
+console.log(myFavoriteMovie.title + ' last for ' + myFavoriteMovie.duration + ' minutes.' + ' Stars: ' + myFavoriteMovie.stars.join())
+
