fbe4d540ff5dc640bd07bca12ef458e94229bab2
salwa alqurashi <Salwa.Kheder.abt.js@tuwaiq.edu.sa>
2021-10-18 18:20:25 +0300

    Create opject.js

---
+++ opject.js
@@ -0,0 +1,6 @@
+const movie = {
+    title: "The Dark Knight",
+    duration: 152,
+    stars: [" Christian Bale", " Heath Ledger", "Aaron Eckhart"]
+}
+console.log(movie.title + " lasts for " + movie.duration + " minutes. " + "Stars: " + movie.stars.join())
