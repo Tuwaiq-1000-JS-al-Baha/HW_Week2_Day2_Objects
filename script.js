0a9c44e1be1c6821606e2bd84b282a87e7fb2548
Najwaageel <Najwa.Ageel.abt.js@tuwaiq.edu.sa>
2021-10-18 19:28:13 +0300

    hw week2 day 2

---
+++ script.js
@@ -0,0 +1,8 @@
+const favoritemovie = 
+{ 
+    title : "The Invisible Man" ,
+    duration : 125 ,
+    stars : [" Elizabeth Moss" ," Oliver Cohen"," Aldis Hodge"," Storm Reid "]
+}
+
+console.log( favoritemovie.title + " lasts for " + favoritemovie.duration + " minutes." + " Stars :" + favoritemovie.stars + " ." )
