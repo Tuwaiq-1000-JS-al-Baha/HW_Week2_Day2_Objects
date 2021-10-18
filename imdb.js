5441318e3a7ed7f72f30cee42211f2e4b10c80a8
mosab10010 <Mosab.Hasen.abt.js@tuwaiq.edu.sa>
2021-10-18 19:21:46 +0300

    home work weak 2 day 2 (IMDB)

---
+++ imdb.js
@@ -0,0 +1,9 @@
+
+let movie =
+{
+title : "Infinite ",
+duration :120,
+stars:["moasb","mohmed","khlid"]
+};
+
+console.log(movie.title + " for " + movie.duration + " min " + "minutes. " + " Stars: "+movie.stars)
