55a287a91770efc253f658db9f7c569c9b590eac
SarahIT200 <Sarah.Ali.abt.js@tuwaiq.edu.sa>
2021-10-18 19:16:25 +0300

    homework week2 day2 commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const movie = {
+    title: "Puff the Magic Dragon",
+    duration: 30,
+    stars: ["Puff", "Jackie", "Living Sneezes"]
+}
+console.log(movie.title + " lasts for " + movie.duration + " minutes. Stars: " + movie.stars.join(", "))
