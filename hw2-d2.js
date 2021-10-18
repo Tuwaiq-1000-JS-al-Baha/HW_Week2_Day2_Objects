edc8c2f83febe0dc0a192ee6cbb6d47e14680161
bader0007 <89877474+bader0007@users.noreply.github.com>
2021-10-18 21:58:33 +0300

    Create hw2-d2.js

---
+++ hw2-d2.js
@@ -0,0 +1,6 @@
+const movie = {
+    name:"Wrath of man",
+    duration: 1.59,
+    stars:["Jason Statham", "Holt McCallany", "Josh Hartnett", ],
+}
+console.log('Puff the ' + movie.name + ' lasts for' + movie.duration +' Stars: ' + movie.stars) 
