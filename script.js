994b1233d42d9738dc6a02aa31536d2c16825669
Abdulathim96 <Abdulathim.Abdullah.abt.js@tuwaiq.edu.sa>
2021-10-18 20:38:53 +0300

    HW_Objects

---
+++ script.js
@@ -0,0 +1,6 @@
+const movie = {
+    name:'1917',
+    duration: 1.59,
+    stars:[' Dean-Charles Chapman ', ' George MacKay ', ' Daniel Mays ', ' Colin Firth '],
+}
+console.log('Puff the ' + movie.name + ' lasts for' + movie.duration +' Stars: ' + movie.stars)
