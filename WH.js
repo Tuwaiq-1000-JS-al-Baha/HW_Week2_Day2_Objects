775f30d4f80d97f02f23a6076b3b64138fd06706
Magdi7021 <92311360+Magdi7021@users.noreply.github.com>
2021-10-18 20:23:15 +0300

    WH.M2

---
+++ WH.js
@@ -0,0 +1,9 @@
+let movie = {title :"Interstellar", duration :169, stars :["Christopher Nolan","Matthew McConaughey","Anne Hathaway","Jessica Chastain","Ellen Burstyn","Michael Caine"]};
+
+    console.log(movie.title + " for " + movie.duration + "min" + "minutes" + "stars: " + movie.stars.join) 
+
+
+
+
+
+
