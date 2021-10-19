3ac8973c15766f477fd5608552158807571aaea8
rahafdm <rahaf.nasser.abt.js@tuwaiq.edu.sa>
2021-10-19 08:34:06 +0300

    HW day 7

---
+++ script.js
@@ -0,0 +1,10 @@
+const movie = {
+    title: " shawshank redemption ",
+    duration: 2.5,
+    stars: [" Andy ", " Samuel Norton ", " Heywood "],
+}
+
+
+
+
+console.log(movie.stars[1] + " the " + movie.title + "lasts for " + movie.duration + " hours " + "stars: " + movie.stars.join(","));
