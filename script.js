b755f41bbcbfecb6d585cd67865751bf818e6610
AmjadMua <92171759+AmjadMua@users.noreply.github.com>
2021-10-19 05:28:24 +0300

    movie-object

---
+++ script.js
@@ -0,0 +1,7 @@
+const movie = {
+    title: "Puff the Magic Dragon",
+    duration: 30,
+    stars: ["Puff ", "Jackie ", "Living Sneezes."]
+}
+
+console.log(movie.title + " last for " + movie.duration + " munites. Stars:" + movie.stars)
