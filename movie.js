e0bc90d510638994c1f6d3610fb6f7cecba16168
RHBSLH <Rehab.Saleh.abt.js@tuwaiq.edu.sa>
2021-10-19 08:40:43 +0300

    hw

---
+++ movie.js
@@ -0,0 +1,9 @@
+const move={
+    title:"Puff the Magic Dragono",
+    duration: 30, 
+    stars : [ "Puff","Jackie","Living Sneezes"]
+}
+for (let i = 0; i < move.stars.length; i++){
+console.log(move.title+" last for "+move.duration+ " minutes " +move.stars[i])
+
+}
