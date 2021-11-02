2b6680b308a6dfa6d04b1a5c0eb1222b53d4bb6f
Wa2999 <92243050+Wa2999@users.noreply.github.com>
2021-11-02 22:20:19 +0300

    done

---
+++ script.js
@@ -0,0 +1,10 @@
+const movie = {
+
+    titel: "YOU",
+    duration: 2.5,
+    stars: ["Doty", "Laf", "Mark"]
+
+}
+
+console.log(movie.titel + " lasts for " + movie.duration +
+    " minutes " + " Stars: " + movie.stars)
